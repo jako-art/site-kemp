7f835b5f8c79f9ade49aedf4a141f54543de64a2
JakeArt <jako.art@yandex.ru>
2025-12-25 16:09:58 +0300

    Еуые 1222

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../components/**/*.stories.@(tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-docs',
