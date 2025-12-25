d9e40ffbc2da7862d288577c6210d87723eb5957
JakeArt <jako.art@yandex.ru>
2025-12-25 16:15:34 +0300

    googgg

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/nextjs-vite';
 
 const config: StorybookConfig = {
-  stories: ['../components/**/*.stories.@(tsx)'],
+  stories: ['../components/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-docs',
